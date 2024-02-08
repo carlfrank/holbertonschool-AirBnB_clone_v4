c8d369ff19e98c3f4d37ddb5dd5f573db1f5d30b
carlfrank <carl-frank7@hotmail.com>
2024-02-08 12:33:54 -0500

    Update CORS configuration for API v1

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    let selectedAmenities = {}; // Object to hold selected amenities
+  
+    $('input[type="checkbox"]').change(function() {
+      if (this.checked) {
+        // Add the amenity ID and name to selectedAmenities
+        selectedAmenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        // Remove the amenity ID from selectedAmenities
+        delete selectedAmenities[$(this).data('id')];
+      }
+  
+      // Update the h4 tag with selected amenities names
+      $('.amenities h4').text(Object.values(selectedAmenities).join(', '));
+    });
+
   // Make an AJAX GET request to the API status endpoint
   $.get('http://0.0.0.0:5001/api/v1/status/', function(data, status) {
       // Check if the request was successful and the API status is "OK"
